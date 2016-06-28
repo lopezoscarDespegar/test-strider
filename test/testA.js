894a82ca51f951a88dc9e596b247c12e2067d17f
Oscar Lopez <oscarlopez@despegar.com>
2016-06-28 15:10:41 -0300

    Test strider test

---
+++ test/testA.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const assert = require("assert");
+const chai = require("chai");
+const expect = chai.expect;
+describe("Simple Test", () => {
+      it("Deberia devolver true",()=>{
+
+return expect(1).to.equal(1);		
+	})
+});
